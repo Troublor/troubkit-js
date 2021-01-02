ac546158d0af33a212bb0cab81be11c32c62493e
Aaron <troublor@live.com>
2021-01-03 00:36:24 +0800

    include dist

--- packages/log/dist/styles/colorful.style.d.ts
+++
@@ -1,5 +0,0 @@
-import { Level } from "../levels";
-export declare abstract class ColorfulStyle {
-    colorful: boolean;
-    protected paintColor(level: Level, str: string): string;
-}
--- packages/log/dist/transports/stdout-transport.d.ts
+++
@@ -1,7 +0,0 @@
-/// <reference types="node" />
-import { LogTransport } from "./tranport";
-declare class stdoutTransport implements LogTransport {
-    transport(): NodeJS.WriteStream;
-}
-export declare const StdoutTransport: stdoutTransport;
-export {};
